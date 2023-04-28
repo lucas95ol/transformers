b97267cd6ece24a727754806380731609502074e
Lucas Oliveira <lucasbarbosadeoliveira95@gmail.com>
2023-04-27 22:15:56 -0300

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 /*variáeis*/ 
 const MENU = document.querySelector(".menu-father");
 const CLICK = document.querySelector(".warning");
+const MINIBTN = document.querySelector(".minibuttons")
+const AUDIO =document.querySelector(".audio");
 const HEAD = document.querySelector(".header");
+const TITLE = document.querySelector(".title")
 const FTEXT = document.querySelector(".first-text");
 const STEXT = document.querySelector(".second-text");
 const TTEXT = document.querySelector(".thirty-text");
-const FTTEXT = document.querySelector(".fourty-text")
+const FTTEXT = document.querySelector(".fourty-text");
 
 /*função para fazer a div que contem o menu aparecer, ela aparecera quando teclar algum botão do teclado*/
 function menu(){
@@ -13,8 +16,16 @@ function menu(){
     hide(CLICK);
 }
 
+function title(){
+    TITLE.setAttribute("style", "animation:title; animation-duration: 5s");
+}
+
+function sides(element){
+    element.setAttribute("style", "visibility: visible; animation: minib; animation-duration: 5s")
+}
+
 function show(element){
-    element.setAttribute("style", "visibility:visible; animation: card; animation-duration: 3s");
+    element.setAttribute("style", "visibility:visible; animation: card; animation-duration: 5s");
 }
 
 function hide(element){
@@ -22,19 +33,33 @@ function hide(element){
 }
 
 function deceptcolor(){
-    HEAD.setAttribute("style","animation: decep; animation-duration: 4s; background-color:  #9d13e7");
+    HEAD.setAttribute("style","animation: decep; animation-duration: 5s; background-color:  #9d13e7");
+    AUDIO.play();
+    sides(MINIBTN);
+    title();
     hide(MENU);
     show(FTEXT);
-    show(STEXT);
+    sides(STEXT);
     show(TTEXT);
     show(FTTEXT);
 }
 
 function autocolor(){
-    HEAD.setAttribute("style","animation: auto; animation-duration: 4s; background-color:  #ed1c33")
+    HEAD.setAttribute("style","animation: auto; animation-duration: 5s; background-color:  #ed1c33");
+    AUDIO.play();
+    sides(MINIBTN);
+    title();
     hide(MENU);
     show(FTEXT);
-    show(STEXT);
+    sides(STEXT);
     show(TTEXT);
     show(FTTEXT);
+}
+
+function red(){
+    HEAD.setAttribute("style", "animation: red; animation-duration: 3s; background-color:  #ed1c33");
+}
+
+function purple(){
+    HEAD.setAttribute("style", "animation: purple; animation-duration: 3s; background-color:   #9d13e7")
 }
